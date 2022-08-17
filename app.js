9b79e9e291345d18650fb6975a1fa3f95be7382e
Andrii Androsovych <kozakizdona@gmail.com>
2022-08-17 13:47:01 +0300

    .
    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import DBinterface  from './database.js';
 import express from 'express';
 import mysql from 'mysql2';
+import CryptoProcedures from './cryptoroutines.js';
 import crypto from 'crypto';
 
 const DATABASE_USER='root';
@@ -38,7 +39,7 @@ app.set('view engine', 'ejs');
         return;
     }
     databaseLayer = new DBinterface(connectionDB);
-    let cryptoProc = new cryptoProcedures(databaseLayer);
+    let cryptoProc = new CryptoProcedures(databaseLayer);
      // console.log(await databaseLayer.writeNewUser({name:'Bill',hashedPassword:'213456',avatar:"abcdefg"}) ); 
     //console.log(await databaseLayer.removeUserByID(16));
    // console.log(await databaseLayer.changeUserPasword({usrId:16, password:123546}) );
@@ -58,91 +59,26 @@ app.set('view engine', 'ejs');
     //console.log( await databaseLayer.clearUserLocked(17) );
     //console.log( await databaseLayer.isUserLocked(17) );
     //console.log( await databaseLayer.isSessionActive(17) );
-    //let res = await databaseLayer.getAllTheChat()
+   let res = await databaseLayer.getAllTheChat()
     //let res = await databaseLayer.getAllUsersWithStatus();
     //let res = await databaseLayer.readKey();
     //await databaseLayer.updateKey({pubKey:123,initVect:456})
    //let res = await databaseLayer.readKey();
     //console.log(res.result.pubKey.toString('hex'));
-   //console.log(await cryptoProc.generateSymmetricCryptoKey());
+    //console.log(await cryptoProc.generateSymmetricCryptoKey());
+    console.log(typeof(Date.now()))
+     await cryptoProc.initInstanceKey();
+     //let x1 = cryptoProc.symmEncrypt(Buffer.from("helloWord"));
+     //let y1 = cryptoProc.symmDecrypt(x1);
+     //console.log(y1.toString("utf-8"));
     
-    await cryptoProc.initInstanceKey()
-    let res = await cryptoProc.encryptData(Buffer.from([0x01,0x02,0x03]))
-    console.log(res);
     
     process.exit(0);
  })
 
 
 
-class cryptoProcedures {
-    constructor (dbInst) {
-        //making a hide property
-      this.privateMembers = new WeakMap();
-      //assign a 'private' - it may be an object
-      this.privateMembers.set(this,{db:dbInst, keyAndVect:{pubKey:0, initVect:0}})    
-    }
-/***call neccessary to init key and vector  */
-    async initInstanceKey () {
-        //get a private member
-        let priv = this.privateMembers.get(this);
-        //read them from the DB
-        let result = await priv.db.readKey();
-        //asssign
-        priv.keyAndVect.pubKey = result.result.pubKey;
-        priv.keyAndVect.initVect = result.result.initVect;
-    }
 
-   
-   async generateSymmetricCryptoKey() {
-        //get a private member of class
-        let priv = this.privateMembers.get(this);
-        let db = priv.db;
-    //it will be using for AES256 symm encryption
-  //generate a symmetric key  
-       let pubKey =  await new Promise((resolve, reject) => {
-            crypto.randomBytes(32, (err, buf) => {
-                if (err) { reject(err) }
-                 resolve(buf);
-              });
-        });
-    //generate an initialization vector
-        let initVect =  await new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) { reject(err) }
-                 resolve(buf);
-              });
-        });
-    // save into DB
-    let res = await db.updateKey({pubKey:pubKey, initVect:initVect});
-    //assign to private members
-    priv.keyAndVect.pubKey = pubKey;
-    priv.keyAndVect.initVect = initVect;
-    return res.result;
-
-    }
- //input and output are buffers
-    async encryptData (data=Buffer.from([0x30,0x31,0x32])) {
-        //create a 
-        let init = this.privateMembers.get(this).keyAndVect;
-        const cipher = crypto.createCipheriv('aes256', init.pubKey, init.initVect);
-        //encrypt
-        let encryptedArray = [cipher.update(data)];
-        //encrypted message returns a buffer
-        encryptedArray.push( cipher.final());
-        return Buffer.concat(encryptedArray);
-    } 
-
- //input and output are buffers
-    async decryptData(encrypted=Buffer.from([0x01,0x02,0x03])){
-               //create a 
-        let init = this.privateMembers.get(this).keyAndVect;
-        const decipher = crypto.createDecipheriv('aes256', init.pubKey, init.initVect);
-        let decryptedMessage = [decipher.update(encrypted)];
-        decryptedMessage.push(decipher.final('utf8'));
-        return Buffer.concat(decryptedMessage);
-    }
-}
  
 
 
---
+++ cryptoroutines.js
@@ -0,0 +1,99 @@
+import crypto from 'crypto';
+
+export default class CryptoProcedures {
+    constructor (dbInst) {
+        //making a hide property
+      this.privateMembers = new WeakMap();
+      //assign a 'private' - it may be an object
+      this.privateMembers.set(this, { 
+        db:dbInst,
+        keyAndVect:{pubKey:0, initVect:0},
+         //input and output are buffers - symmetrical encription
+        _encryptData:    (data=Buffer.from([0x30,0x31,0x32]))=> {
+            
+            let init = this.privateMembers.get(this).keyAndVect;
+            const cipher = crypto.createCipheriv('aes256', init.pubKey, init.initVect);
+            //encrypt
+            let encryptedArray = [cipher.update(data)];
+            //encrypted message returns a buffer
+            encryptedArray.push( cipher.final());
+            return Buffer.concat(encryptedArray);
+        },
+        //input and output are buffers - symmetrical decryption
+            _decryptData:  (encrypted=Buffer.from([0x01,0x02,0x03]))=>{
+                
+            let init = this.privateMembers.get(this).keyAndVect;
+            const decipher = crypto.createDecipheriv('aes256', init.pubKey, init.initVect);
+            let decryptedMessage = [decipher.update(encrypted)];
+            decryptedMessage.push(decipher.final( ));
+            return Buffer.concat(decryptedMessage);
+        },
+
+        })    
+    }
+/***call neccessary to init key and vector  */
+    async initInstanceKey () {
+        //get a private member
+        let priv = this.privateMembers.get(this);
+        //read them from the DB
+        let result = await priv.db.readKey();
+        //asssign
+        priv.keyAndVect.pubKey = result.result.pubKey;
+        priv.keyAndVect.initVect = result.result.initVect;
+    }
+
+   
+   async generateSymmetricCryptoKey() {
+        //get a private member of class
+        let priv = this.privateMembers.get(this);
+        let db = priv.db;
+
+
+    //it will be using for AES256 symm encryption
+  //generate a symmetric key  
+       let pubKey =  await new Promise((resolve, reject) => {
+            crypto.randomBytes(32, (err, buf) => {
+                if (err) { reject(err) }
+                 resolve(buf);
+              });
+        });
+    //generate salt
+        let salt =  await new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) { reject(err) }
+                 resolve(buf);
+              });
+        });
+    //generate IV
+
+        let iv = await  new Promise((resolve, reject) => {
+            crypto.pbkdf2(pubKey, salt, 100000, 16, 'sha512', (err, derivedKey)=>{
+                if (err) { reject(err) }
+                resolve(derivedKey);
+            })
+        });
+
+        // save into DB
+        let res = await db.updateKey({pubKey:pubKey, initVect:iv});
+        //assign to private members
+        priv.keyAndVect.pubKey = pubKey;
+        priv.keyAndVect.initVect = iv;
+        return res.result;
+
+    }
+ //input and output are buffers - symmetrical encription
+     symmEncrypt(data=Buffer.from([0x30,0x31,0x32])) {
+        
+        let init = this.privateMembers.get(this);
+        return init._encryptData(data);
+    } 
+
+ //input and output are buffers - symmetrical decryption
+     symmDecrypt(encrypted=Buffer.from([0x01,0x02,0x03])){
+               
+        let init = this.privateMembers.get(this);
+       return init._decryptData(encrypted);
+    }
+
+
+}
