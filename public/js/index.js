cef9c1e72e3f9e0345b4bb7df67c78a622539a54
Andrii Androsovych <kozakizdona@gmail.com>
2022-10-22 12:15:30 +0300

    *
    *

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,12 @@ class ScrollAnimation {
  #scaleEffect;
  #opacity;
  #position;
- #onScrollListener = () => {
-   
-    this.#targetNode.style.bottom =  `${(window.scrollY | 0) * 3}px`; 
-    this.#targetNode.style.opacity = 1 - ((1/(window.innerHeight/3)) * (window.scrollY+1));
-    console.log((1/window.innerHeight) * (window.scrollY+1));
- }
+    #onScrollListener = () => {
+    
+        this.#targetNode.style.bottom =  `${(window.scrollY | 0) * 3}px`; 
+        this.#targetNode.style.opacity = 1 - ((1/(window.innerHeight/3)) * (window.scrollY+1));
+        console.log((1/window.innerHeight) * (window.scrollY+1));
+    }
     constructor (nodeToAnim,scaleEffect=1.2) {
                  this.#scaleEffect = scaleEffect;
                  this.#targetNode = nodeToAnim;
