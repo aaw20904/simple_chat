978872380a0a8678857bea33c45ec44779bb887d
Andrii Androsovych <kozakizdona@gmail.com>
2022-10-27 14:53:55 +0300

    *
    *

---
+++ public/js/index_old.js
@@ -0,0 +1,24 @@
+class ScrollAnimation {
+ #targetNode;
+ #scaleEffect;
+ #opacity;
+ #position;
+    #onScrollListener = () => {
+    
+        this.#targetNode.style.bottom =  `${(window.scrollY | 0) * 3}px`; 
+        this.#targetNode.style.opacity = 1 - ((1/(window.innerHeight/3)) * (window.scrollY+1));
+        console.log((1/window.innerHeight) * (window.scrollY+1));
+    }
+    constructor (nodeToAnim,scaleEffect=1.2) {
+                 this.#scaleEffect = scaleEffect;
+                 this.#targetNode = nodeToAnim;
+                this.#targetNode.classList.add('scroll_animation');
+                 this.#targetNode.style.left = '50vw';
+                 window.addEventListener('scroll',this.#onScrollListener);
+    }
+
+}
+
+window.onload = () =>{
+    let animation = new ScrollAnimation(document.querySelector('.arrow'),null);
+}
